5ecaf494c8309c3ce60bd002e2859e922182ea5c
TobeyChao <Coolfire1314@outlook.com>
2018-10-10 16:54:45 +0800

    io

--- CPlusPlus_Learn/vector.cpp
+++ CPlusPlus_Learn/vector.cpp
@@ -1,17 +1,16 @@
-//#include <iostream>
-//#include <string>
-//#include <vector>
-//
-//using namespace std;
-//
-//int main()
-//{
-//	vector<string> stringList;
-//	string str;
-//	while (cin >> str)
-//	{
-//		stringList.push_back(str);
-//	}
-//	system("pause");
-//	return 0;
-//}
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector<string> stringList;
+	string str;
+	while (cin >> str)
+	{
+		stringList.push_back(str);
+	}
+	return 0;
+}
