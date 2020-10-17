02e188b48c450f3559df2c1473ecdeae7f30da0d
TobeyChao <coolfire1314@outlook.com>
2020-10-17 17:44:56 +0800

    新东方/cast

--- OL-Program/平均碎片长度cpp.cpp
+++ OL-Program/平均碎片长度cpp.cpp
@@ -1,22 +1,22 @@
-#include <string>
-#include <iostream>
-#include <iomanip>
-
-int main(int argc, char* argv[])
-{
-	std::string str;
-	std::cin >> str;
-	int count = 1;
-	float sum = str.size();
-	for (int i = 0; i < str.size() - 1; ++i)
-	{
-		
-		if (str[i] != str[i + 1])
-		{
-			++count;
-		}
-	}
-	std::cout << std::setprecision(3) << sum / count;
-
-	return 0;
-}
+//#include <string>
+//#include <iostream>
+//#include <iomanip>
+//
+//int main(int argc, char* argv[])
+//{
+//	std::string str;
+//	std::cin >> str;
+//	int count = 1;
+//	float sum = str.size();
+//	for (int i = 0; i < str.size() - 1; ++i)
+//	{
+//		
+//		if (str[i] != str[i + 1])
+//		{
+//			++count;
+//		}
+//	}
+//	std::cout << std::setprecision(3) << sum / count;
+//
+//	return 0;
+//}
---
+++ OL-Program/题目1.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+using namespace std;
+
+//int main()
+//{
+//    int num;
+//    cin >> num;
+//    std::vector<int> vec1;
+//    string str;
+//    getline(cin, str);
+//    getline(cin, str);
+//    int num1 = 0;
+//    for (auto ch : str)
+//    {
+//        if (isdigit(ch))
+//        {
+//            vec1.push_back(ch - '0');
+//        }
+//    }
+//    string str2;
+//    getline(cin, str2);
+//    for (auto ch : str2)
+//    {
+//        if (isdigit(ch))
+//        {
+//            vec1.push_back(ch - '0');
+//        }
+//    }
+//    sort(vec1.begin(), vec1.end());
+//    int res = (vec1[num] + vec1[num - 1]) / 2.0f;
+//	return 0;
+//}
+
+int MaxSubSequence(vector<int> A, int N)
+{
+    int ThisSum, MaxSum, j;
+    ThisSum = MaxSum = 0;
+    for (j = 0; j < N; j++)
+    {
+        ThisSum += A[j];
+
+        if (ThisSum > MaxSum)
+            MaxSum = ThisSum;
+        else if (ThisSum < 0)
+            ThisSum = 0;
+    }
+    return MaxSum;
+}
+
+
+int main()
+{
+    int num;
+    cin >> num;
+    std::vector<int> vec1;
+    string str;
+    getline(cin, str);
+    getline(cin, str);
+    int num1 = 0;
+    for (auto ch : str)
+    {
+        if (isdigit(ch))
+        {
+            vec1.push_back(ch - '0');
+        }
+    }
+    cout << MaxSubSequence(vec1, num);
+    return 0;
+}
