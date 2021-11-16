65dd9bcb1f009cfc5dc065aa1ce4eae27c612bda
tobeychao <coolfire1314@outlook.com>
2021-11-16 13:28:03 +0800

    leetcode 82 83删除链表重复元素

---
+++ LeetCode/83.删除排序链表中的重复元素.cpp
@@ -0,0 +1,43 @@
+//struct ListNode {
+//	int val;
+//	ListNode* next;
+//	ListNode() : val(0), next(nullptr) {}
+//	ListNode(int x) : val(x), next(nullptr) {}
+//	ListNode(int x, ListNode* next) : val(x), next(next) {}
+//};
+//
+//class Solution {
+//public:
+//	ListNode* deleteDuplicates(ListNode* head) {
+//		ListNode* dummyNode = new ListNode();
+//		dummyNode->next = head;
+//		ListNode* node = dummyNode;
+//		while (node->next && node->next->next)
+//		{
+//			if (node->next->val == node->next->next->val)
+//			{
+//				ListNode* toDel = node->next->next;
+//				node->next->next = toDel->next;
+//				delete toDel;
+//			}
+//			else
+//			{
+//				node = node->next;
+//			}
+//		}
+//		ListNode* newHead = dummyNode->next;
+//		delete dummyNode;
+//		return newHead;
+//	}
+//};
+//
+//int main()
+//{
+//    ListNode* node = new ListNode(1);
+//    node->next = new ListNode(1);
+//	node->next->next = new ListNode(1);
+//	node->next->next->next = new ListNode(1);
+//	Solution s;
+//	s.deleteDuplicates(node);
+//	return 0;
+//}
--- LeetCode/92.反转链表 II.cpp
+++ LeetCode/92.反转链表 II.cpp
@@ -1,81 +1,81 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-
-struct ListNode {
-	int val;
-	ListNode* next;
-	ListNode() : val(0), next(nullptr) {}
-	ListNode(int x) : val(x), next(nullptr) {}
-	ListNode(int x, ListNode* next) : val(x), next(next) {}
-};
-
-class Solution {
-public:
-	ListNode* reverseBetween(ListNode* head, int left, int right) {
-		if (left == right == 1)
-		{
-			return head;
-		}
-		ListNode* ret = head;
-		ListNode* node = head;
-		int leftTmp = left;
-		// 开始翻转的前一个节点
-		ListNode* nodeReverseBeginNode = node;
-		while (--leftTmp)
-		{
-			nodeReverseBeginNode = node;
-			node = node->next;
-		}
-
-		int reverseCount = right - left;
-
-		ListNode* subhead = node;
-		ListNode* subtail = node;
-		node = node->next;
-		subhead->next = nullptr;
-		for (int i = 0; i < reverseCount; i++)
-		{
-			ListNode* newNode = node;
-			node = node->next;
-			newNode->next = subhead;
-			subhead = newNode;
-		}
-
-		// 防止不移动的情况
-		if (nodeReverseBeginNode != subhead)
-		{
-			// 接上尾巴
-			subtail->next = node;
-			// 接上头
-			if (left != 1)
-			{
-				nodeReverseBeginNode->next = subhead;
-			}
-		}
-
-		if (left == 1)
-		{
-			ret = subhead;
-		}
-
-		return ret;
-	}
-};
-
-int main()
-{
-    ListNode* node = new ListNode(1);
-    node->next = new ListNode(2);
-	node->next->next = new ListNode(3);
-	Solution s;
-	s.reverseBetween(node, 2,2);
-	return 0;
-}
+///**
+// * Definition for singly-linked list.
+// * struct ListNode {
+// *     int val;
+// *     ListNode *next;
+// *     ListNode() : val(0), next(nullptr) {}
+// *     ListNode(int x) : val(x), next(nullptr) {}
+// *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+// * };
+// */
+//
+//struct ListNode {
+//	int val;
+//	ListNode* next;
+//	ListNode() : val(0), next(nullptr) {}
+//	ListNode(int x) : val(x), next(nullptr) {}
+//	ListNode(int x, ListNode* next) : val(x), next(next) {}
+//};
+//
+//class Solution {
+//public:
+//	ListNode* reverseBetween(ListNode* head, int left, int right) {
+//		if (left == right == 1)
+//		{
+//			return head;
+//		}
+//		ListNode* ret = head;
+//		ListNode* node = head;
+//		int leftTmp = left;
+//		// 开始翻转的前一个节点
+//		ListNode* nodeReverseBeginNode = node;
+//		while (--leftTmp)
+//		{
+//			nodeReverseBeginNode = node;
+//			node = node->next;
+//		}
+//
+//		int reverseCount = right - left;
+//
+//		ListNode* subhead = node;
+//		ListNode* subtail = node;
+//		node = node->next;
+//		subhead->next = nullptr;
+//		for (int i = 0; i < reverseCount; i++)
+//		{
+//			ListNode* newNode = node;
+//			node = node->next;
+//			newNode->next = subhead;
+//			subhead = newNode;
+//		}
+//
+//		// 防止不移动的情况
+//		if (nodeReverseBeginNode != subhead)
+//		{
+//			// 接上尾巴
+//			subtail->next = node;
+//			// 接上头
+//			if (left != 1)
+//			{
+//				nodeReverseBeginNode->next = subhead;
+//			}
+//		}
+//
+//		if (left == 1)
+//		{
+//			ret = subhead;
+//		}
+//
+//		return ret;
+//	}
+//};
+//
+//int main()
+//{
+//    ListNode* node = new ListNode(1);
+//    node->next = new ListNode(2);
+//	node->next->next = new ListNode(3);
+//	Solution s;
+//	s.reverseBetween(node, 2,2);
+//	return 0;
+//}
