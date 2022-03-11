3eb8b578e98b7bda6cf3535895e393f7fad0f3ae
TobeyChao <tobeychao@qq.com>
2022-03-11 19:00:42 +0800

    extern 测试

---
+++ CPlusPlus_Learn/et2.cpp
@@ -0,0 +1,5 @@
+int index = 100;
+int fun2()
+{
+	return index;
+}
--- CPlusPlus_Learn/queue.cpp
+++ CPlusPlus_Learn/queue.cpp
@@ -1,23 +1,23 @@
-#include <iostream>
-#include <string>
-#include <queue>
-using namespace std;
-int main()
-{
-	queue<string> q;
-	q.push("hello2");
-	q.push("hello3");
-	q.push("hello4");
-	q.push("hello5");
-	q.push("hello8");
-	q.push("hello7");
-	q.push("hello9");
-
-
-	while (!q.empty())
-	{
-		cout << q.front() << endl;
-		q.pop();
-	}
-	return 0;
-}
+//#include <iostream>
+//#include <string>
+//#include <queue>
+//using namespace std;
+//int main()
+//{
+//	queue<string> q;
+//	q.push("hello2");
+//	q.push("hello3");
+//	q.push("hello4");
+//	q.push("hello5");
+//	q.push("hello8");
+//	q.push("hello7");
+//	q.push("hello9");
+//
+//
+//	while (!q.empty())
+//	{
+//		cout << q.front() << endl;
+//		q.pop();
+//	}
+//	return 0;
+//}
